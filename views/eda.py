55fbc6052e9c87a438039acf9de3f4d1abd717e5
TwangGiaHan <22520394@gm.uit.edu.vn>
2024-09-05 23:54:41 +0700

    update eda.py

--- views/eda.py
+++ views/eda.py
@@ -6,15 +6,13 @@ from sklearn import datasets
 
 from sklearn import datasets
 import matplotlib.pyplot as plt
-# import pandas_bokeh
-# import missingno
 import seaborn as sns
 
 
 st.title('Automated EDA Dashboard  :bar_chart: :computer: :coffee:')
 st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. As a sample example, you can upload famous <a href='https://www.kaggle.com/competitions/titanic/data?select=train.csv'>Titanic Dataset</a> available from Kaggle.", unsafe_allow_html=True)
 upload = st.file_uploader(label='Upload File Here: ', type= ["csv"])
-
+st.write("The dataset used for this demo is the Titanic dataset from <a href='https://github.com/datasciencedojo/datasets/blob/master/titanic.csv'>GitHub</a> ", unsafe_allow_html=True)
 def create_correlation_chart(corr_df):    ## Create Correlation Chart using Matplotlib
     fig = plt.figure(figsize=(10,10))
     plt.imshow(corr_df.values, cmap="Blues")
@@ -127,3 +125,29 @@ with tab2: ## Individual Column Stats
     plt.xticks(rotation=45)
     plt.grid(True)
     st.pyplot(cat_fig, use_container_width=True)
+
+with tab3:  ## Explore Relation Between Features
+    st.subheader('Explore Relationship Between Features of Dataset')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        x_axis = st.selectbox(label="A-Axis", options= cont_columns, index=0)
+    with col2:
+        y_axis = st.selectbox(label="Y-Axis", options= cat_columns, index=0)
+
+    # Optional color encoding using another categorical feature
+    color_encode = st.selectbox(label="Color-Encode", options=[None,] + cat_columns)
+
+    plt.figure(figsize=(10, 6))
+
+    if color_encode:
+        sns.scatterplot(data=df, x=x_axis, y=y_axis, hue=color_encode, palette='coolwarm', s=100)
+    else:
+        sns.scatterplot(data=df, x=x_axis, y=y_axis, s=100)
+
+    plt.title(f"{x_axis.capitalize()} vs {y_axis.capitalize()}", fontsize=16)
+    plt.xlabel(x_axis.capitalize(), fontsize=14)
+    plt.ylabel(y_axis.capitalize(), fontsize=14)
+
+    # Display the scatter plot in Streamlit
+    st.pyplot(plt)
